09bd8109aa4cc1bcd6a8187183ca73fd91441434
Vernon Louie <vernonlouie@aol.com>
2016-11-30 15:46:28 -0800

    messing with main.js -VL

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 
 $(document).ready(function () {
-    $(".square").click(square_clicked);         // Call function square_clicked when clicking on a number button
+    $(".cell").click(cell_clicked);         // Call function cell_clicked when clicking on a number button
 
 });
 
-function square_clicked () {
+function cell_clicked () {
+    var cell_index = $(this).text();
+    cell_index = cell_index.trim();         // .trim Removes white space from the string/text.
+    console.log("in cell_clicked and cell_index is: " + cell_index);
 
-    var square_index = $(this).text();
-    square_index = square_index.trim();         // .trim Removes white space from the string/text.
-    console.log("in square clicked and square_index is: " + square_index);
+    $(this).text
 
     /* console.log("numString: " + numString);
      var isItEmpty = array1[index];
@@ -27,7 +28,7 @@ function square_clicked () {
      */
 } // end of function number_clicked
 
-square_template = function () {
+var square_template = function () {
     this.make_X = function() {
         $(this).text('X');
     }
@@ -39,64 +40,11 @@ square_template = function () {
     }
 }
 
-player_template = function () {
-    this.name = "Cung";
-    this.games_played = 3;
-    this.games_won = 4;
-    this.one_owned;
-    this.two_owned;
-    this.three_owned;
-    this.A_owned;
-    this.B_owned;
-    this.C_owned;
-    this.increase_A_by_one();
-    this.increase_B_by_one();
-    this.increase_C_by_one();
-}$(document).ready(function () {
-    $(".square").click(square_clicked);         // Call function number_clicked when clicking on a number button
-
-});
-
-
-function square_clicked () {
-
-    var square_index = $(this).text();
-    square_index = square_index.trim();         // .trim Removes white space from the string/text.
-    console.log("in square clicked and square_index is: " + square_index);
-
-    /* console.log("numString: " + numString);
-     var isItEmpty = array1[index];
-
-     if (isItEmpty === " ") {
-     array1[index] = numString;
-     var new_h4 = $("<h4>", {
-     text: " " + array1[index] + " "
-     });
-     $(".container1 .display").append(new_h4);
-     } else {
-     array1[index] = array1[index] + numString;
-     // console.log("index: " + index + array1[index]);
-     $(".container1 .display h4:last-child").text(array1[index]);
-     }
-     */
-} // end of function number_clicked
-
-square_template = function () {
-    this.make_X = function() {
-        $(this).text('X');
-    }
-    this.make_O = function() {
-        $(this).text('O');
-    }
-    this.clearXO = function() {
-        $(this).text(' ');
-    }
-}
-
-player_template = function () {
-    this.name = "Cung";
-    this.games_played = 3;
-    this.games_won = 4;
+/*
+var player_template = function () {
+    this.name;
+    this.games_played;
+    this.games_won;
     this.one_owned;
     this.two_owned;
     this.three_owned;
@@ -107,6 +55,12 @@ player_template = function () {
     this.increase_B_by_one();
     this.increase_C_by_one();
 }
+
+var player1 = new player_template()
+
+player1.name = "Cung";
+
+*/
 
 function start_game (){
 
